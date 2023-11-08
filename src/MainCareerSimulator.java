5e4d967d6ede7bf1b7363427f04639711f0e5c38
BT_4N2_20 <irsals@nycstudents.net>
2023-11-08 14:02:28 -0500

    Was able to make the code loop so that user can simulate through multiple yeara dn get their stats for the years as well as final stats

--- src/MainCareerSimulator.java
+++ src/MainCareerSimulator.java
@@ -21,12 +21,32 @@ public class MainCareerSimulator {
 
         //Age
         int age = 16;
-
+        int totalGoals = 0;
+        int totalAssists = 0;
+        int totalGames = 0;
+        int totalCleanSheets = 0;
+        int awards = 0;
         //Position
 
         String position = ChooseTraits.choosePosition();
-        SoccerPlayerCareer newPlayer = new SoccerPlayerCareer(name, age, nationality, position, currentTeam);
-        System.out.print(newPlayer.simulateYear());
+        SoccerPlayerCareer newPlayer = new SoccerPlayerCareer(name, age, nationality, position, currentTeam, totalGoals, totalAssists, totalGames, totalCleanSheets, awards);
+        System.out.println(newPlayer.simulateYear());
+
+        System.out.print("Simulate year? Enter 1: ");
+        String input = s.nextLine();
+        int simulate = Integer.parseInt(input);
+        while (newPlayer.activeCareer()){
+            if (simulate == 1){
+                System.out.println(newPlayer.simulateYear());
+                System.out.print("Simulate year? Enter 1: ");
+                input = s.nextLine();
+                simulate = Integer.parseInt(input);
+            }
+        }
+        System.out.print(newPlayer.toString());
+
+
+
 
 
     }
--- src/SoccerPlayerCareer.java
+++ src/SoccerPlayerCareer.java
@@ -14,38 +14,63 @@ private int age;
 private String nationality;
 private String position;
 
+private int totalGoals;
+private int totalAssists;
+private int totalCleanSheets;
+private int totalGames;
+private int awards;
 
 private String currentTeam;
-    public SoccerPlayerCareer(String playerName, int age, String nationality, String position, String currentTeam){
+    public SoccerPlayerCareer(String playerName, int age, String nationality, String position, String currentTeam, int totalGoals, int totalAssists, int totalGames, int totalCleanSheets, int awards){
         this.playerName = playerName;
         this.age = age;
         this.nationality = nationality;
         this.position = position;
         this.currentTeam = currentTeam;
+        this.totalGoals = totalGoals;
+        this.totalAssists = totalAssists;
+        this.totalGames = totalGames;
+        this.totalCleanSheets = totalCleanSheets;
+        this.awards = awards;
     }
 
-    public String activeCareer(){
-        int totalGoals = 0;
-        int totalAssists = 0;
-        int totalGamesPlayed = 0;
 
-        while (age < 18){
-
+// Active career will allow the program to run as long as the playing boolean is true, otherwise it will return false
+    public Boolean activeCareer(){
+        boolean playing = true;
+        if (age == 36){
+            playing = false;
         }
-        return "career finished";
-
+        return playing;
     }
+
+    //Simulate Year method uses the stats for season that randomly generate the games and stats for the player depending on their position and returns it
+    // It also increases the age
     public String simulateYear(){
         String stats = "";
+
         stats = statsForSeason();
         age++;
+        if (bestPlayer()){
+            awards++;
+        }
         return stats + "\nAge: " + age;
     }
 
-    public String changeTeam(){
-        currentTeam = ChooseTraits.printTeamOptions();
-        return currentTeam;
+
+
+    public boolean bestPlayer(){
+        int yourVotes = (int)(Math.random()*60)+1;
+        int opponentOneVotes = (int)(Math.random()*60)+1;
+        int opponentTwoVotes = (int)(Math.random()*60)+1;
+        if (yourVotes > opponentOneVotes && yourVotes > opponentTwoVotes){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
+
 
 
     public String statsForSeason(){
@@ -55,37 +80,51 @@ private String currentTeam;
             int goals = (int)(Math.random()*60)+1;
             int assists = (int)(Math.random()*21);
             stats = "Games: " + games + "\nGoals: " + goals + "\nassists: " + assists;
+            totalGames = totalGames + games;
+            totalGoals = totalGoals + goals;
+            totalAssists = totalAssists + assists;
         }
         else if (position == "Midfielder"){
             int games = 50;
             int goals = (int)(Math.random()*20)+1;
             int assists = (int)(Math.random()*41);
             stats = "Games: " + games + "\nGoals: " + goals + "\nassists: " + assists;
+            totalGames = totalGames + games;
+            totalGoals = totalGoals + goals;
+            totalAssists = totalAssists + assists;
         }
         else if (position == "Defender"){
             int games = 50;
             int goals = (int)(Math.random()*10)+1;
             int assists = (int)(Math.random()*15);
             stats = "Games: " + games + "\nGoals: " + goals + "\nassists: " + assists;
+            totalGames = totalGames + games;
+            totalGoals = totalGoals + goals;
+            totalAssists = totalAssists + assists;
         }
         else if (position == "Goalkeeper"){
             int games = 50;
             int cleanSheets = (int)(Math.random()*51);
             stats = "Games: " + games + "\nClean Sheets: " + cleanSheets;
+            totalCleanSheets = totalCleanSheets + cleanSheets;
         }
         return stats;
 
     }
-    public boolean retire(){
-        System.out.print("Do you wish to retire (y or n)");
-        String retire = s.nextLine();
-        if (retire == "n"){
-            return true;
+    public String toString(){
+        if (position == "Attacker" && position == "Midfielder" && position == "Defender"){
+            return "Your final stats" +
+                    "\nGames: " + totalGames +
+                    "\nGoals: " + totalGoals +
+                    "\nAssists: " + totalAssists +
+                    "\nGoal Ratio: " + totalGoals/totalGames +
+                    "\nAssist Ratio" + totalAssists/totalGames;
         }
-        return false;
+        else{
+            return "Your final stats" +
+                    "\nGames: " + totalGames +
+                    "\nClean Sheets: " + totalCleanSheets +
+                    "\nClean Sheet Ratio: " + totalGames/totalCleanSheets;
+        }
     }
-
-
-
-    
 }
